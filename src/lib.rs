21cf8611e4cb37dcfe945454cadec9254ccb066d
Gé Vissers <gvissers@gmail.com>
2021-05-17 12:54:23 +0200

    Fix copyright header

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2019 Q1t BV
+// Copyright, 2021, Gé Vissers
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
